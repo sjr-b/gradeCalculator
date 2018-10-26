00213c061f4f65712dede133d4dafc98f7d6f57e
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-10-26 14:17:27 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// NO GLOBAL VARIABLES. EVER. DON'T EVEN THINK ABOUT IT.
+
+/* calculateCurrentGrade() → takes data from page, calls on sub-functions to calculate the student grade and output it
+back to page.  Also “return” the result so that calculateGradeNeeded() can use it. */
+function calculateCurrentGrade(){
+
+}
+
+/* addRow() Adds a new category/weight row (up to 6) */
+function addRow(){
+
+}
+
+/* convertArrayStringToNumber(string) → takes a string of comma separated values (from page) and returns it as an array of
+numbers.  Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each
+item in the array into a number like: array[i] = parseInt(array[i]) */
+function convertArrayStringToNumber(){
+
+}
+
+/* averageArray() → takes an array of numbers and returns the average of those numbers */
+function averageArray(){
+
+}
+
+/* calculateGradeNeeded() → takes the current grade returned by calculateCurrentGrade() and the grade desired and does
+the math to determine what the user needs on the final. */
+function calculateGradeNeeded(){
+
+}
