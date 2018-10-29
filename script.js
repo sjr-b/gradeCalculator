e1107e20a5f6b6b62c45667f6bd0e0e2ea1fb15e
“sjr-bglobal user.email sophier-b@sbcglobal.netgit config --global user.name “sjr-b <sophier-b@sbcglobal.netgit config --global user.name sjr-b>
2018-10-28 22:07:22 -0700

    try to figure out how to get the information from the text box to the button and addRow()

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 // NO GLOBAL VARIABLES. EVER. DON'T EVEN THINK ABOUT IT.
 
-/* calculateCurrentGrade() → takes data from page, calls on sub-functions to calculate the student grade and output it
-back to page.  Also “return” the result so that calculateGradeNeeded() can use it. */
-function calculateCurrentGrade(){
-
+function setUpPage(){
+    var rowCount = 0;
+    var firstSection = "Homework";
+    addRow(rowCount, firstSection);
+    var sectionTitle = document.getElementById("userInput").value;
 }
 
 /* addRow() Adds a new category/weight row (up to 6) */
-function addRow(){
+function addRow(rowCount, input){
+    if(rowCount <= 6){
+        // these are the variables for the table (oh-so-creatively named after the html code)
+        var row = document.createElement("tr");
+        var data = document.createElement("td");
 
+        // this is where the rows and data are appended
+        document.getElementById("gradeWeight").appendChild(row);
+        row.append(data);
+
+        // these inputs the border
+        document.getElementById("gradeWeight").style.border = "1px solid black";
+        data.style.border = "1px solid black";
+
+        // this helps make sure that there are only six sections.
+        data.innerHTML = input;
+        rowCount++;
+    }
 }
 
 /* convertArrayStringToNumber(string) → takes a string of comma separated values (from page) and returns it as an array of
@@ -21,6 +38,11 @@ function convertArrayStringToNumber(){
 /* averageArray() → takes an array of numbers and returns the average of those numbers */
 function averageArray(){
 
+}
+
+/* calculateCurrentGrade() → takes data from page, calls on sub-functions to calculate the student grade and output it
+back to page.  Also “return” the result so that calculateGradeNeeded() can use it. */
+function calculateCurrentGrade(){
 }
 
 /* calculateGradeNeeded() → takes the current grade returned by calculateCurrentGrade() and the grade desired and does
