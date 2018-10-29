773c71227f1063f2e7c372d73feb74ab2d46e2b0
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-10-29 14:41:52 -0700

    het text id to work and other stuff as well

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 // NO GLOBAL VARIABLES. EVER. DON'T EVEN THINK ABOUT IT.
 
-/* calculateCurrentGrade() → takes data from page, calls on sub-functions to calculate the student grade and output it
-back to page.  Also “return” the result so that calculateGradeNeeded() can use it. */
-function calculateCurrentGrade(){
-
+// This is the onload function, and happens when the page is loaded.
+function onLoad(){
+    addRow();
 }
 
-/* addRow() Adds a new category/weight row (up to 6) */
+// addRow() Adds a new category/weight row (up to 6)
 function addRow(){
+    var row = document.createElement("tr");
+    var firstData = document.createElement("td");
+    var emptyBetween = document.createElement("td");
+    var secondData = document.createElement("td");
+    var categoryTitle = document.getElementsByTagName("input").value;
+    document.getElementById("categories").appendChild(row);
+    row.appendChild(firstData);
+    row.appendChild(emptyBetween);
+    row.appendChild(secondData);
+    firstData.innerHTML = categoryTitle;
+    firstData.innerText = "test";
+    secondData.innerText = "Percentage";
+    firstData.style.backgroundColor = "red";
+    firstData.style.length = "25px";
+    secondData.style.backgroundColor = "red";
 
+    var buttonPlace = document.getElementById("firstBreak");
+    var button = document.createElement("button");
+    var buttonMessage = document.createTextNode("Add A Category");
+    document.insertBefore(buttonPlace, button);
+    button.innerHTML = buttonMessage;
+
+    var textInput = document.createElement("input");
+    textInput.setAttribute("type", "text");
+    textInput.setAttribute("value", "");
+    button.insertBefore(button, textInput);
 }
 
 /* convertArrayStringToNumber(string) → takes a string of comma separated values (from page) and returns it as an array of
@@ -18,8 +42,14 @@ function convertArrayStringToNumber(){
 
 }
 
-/* averageArray() → takes an array of numbers and returns the average of those numbers */
+// averageArray() → takes an array of numbers and returns the average of those numbers
 function averageArray(){
+
+}
+
+/* calculateCurrentGrade() → takes data from page, calls on sub-functions to calculate the student grade and output it
+back to page.  Also “return” the result so that calculateGradeNeeded() can use it. */
+function calculateCurrentGrade(){
 
 }
 
