dfc91b7ec4db6c9a81f88cae4f18c4d3aecd05dd
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-10-31 14:18:50 -0700

    complete second function

--- script.js
+++ script.js
@@ -1,61 +1,74 @@
 // NO GLOBAL VARIABLES. EVER. DON'T EVEN THINK ABOUT IT.
 
-// This is the onload function, and happens when the page is loaded.
+// This is the automatic function, and happens when the page is loaded.
 function onLoad(){
     addRow();
 }
 
 // addRow() Adds a new category/weight row (up to 6)
 function addRow(){
-    // these are the variables
-    var firstRow = document.createElement("tr");
-    var firstData = document.createElement("td");
-    firstData.setAttribute("class", "catTitle");
-    var secondData = document.createElement("td");
-    if (document.getElementById("categoryTitle").value == ""){
-        var categoryTitle = "Homework"
-    } else {
-        var categoryTitle = document.getElementById("categoryTitle").value;
+    if (document.getElementById("categories").rows.length < 12){
+        // these are the variables
+        var titleRow = document.createElement("tr");
+
+        var firstTitle = document.createElement("td");
+        firstTitle.setAttribute("class", "title");
+        var secondTitle = document.createElement("td");
+        secondTitle.setAttribute("class", "title");
+        if (document.getElementById("categoryTitle").value == ""){
+            var categoryTitle = "Homework";
+        } else {
+            var categoryTitle = document.getElementById("categoryTitle").value
+            document.getElementById("categoryTitle").value = "";
+        }
+
+        var inputRow = document.createElement("tr");
+
+        var firstInputContainer = document.createElement("td");
+        firstInputContainer.setAttribute("class", "inputCont");
+        var secondInputContainer = document.createElement("td");
+        secondInputContainer.setAttribute("class", "inputCont");
+
+        var scoreInput = document.createElement("input");
+        scoreInput.setAttribute("class", "input");
+        var weightInput = document.createElement("input");
+        weightInput.setAttribute("class", "input");
+
+        // this is where everything in the first row is appended
+        titleRow.appendChild(firstTitle);
+        titleRow.appendChild(secondTitle);
+        document.getElementById("categories").appendChild(titleRow);
+
+        // this is where everything in the second row is appended
+        firstInputContainer.appendChild(scoreInput);
+        secondInputContainer.appendChild(weightInput);
+        inputRow.appendChild(firstInputContainer);
+        inputRow.appendChild(secondInputContainer);
+        document.getElementById("categories").appendChild(inputRow);
+
+        // this is where everything is given its names and classes
+        firstTitle.innerHTML = categoryTitle + " Scores";
+        secondTitle.innerText = "Percentage Weight";
     }
-    secondData.setAttribute("class", "perTitle")
-    var secondRow = document.createElement("tr");
-    var secondFirstData = document.createElement("tr");
-    var secondSecondData = document.createElement("tr");
-    var inputScores = document.createElement("input");
-    inputScores.setAttribute("id", categoryTitle + "scores");
-    inputScores.setAttribute("class", "inp");
-    var inputWeight = document.createElement("input");
-    inputWeight.setAttribute("id", categoryTitle + "weight");
-    inputWeight.setAttribute("class", "inp");
-
-    // this is where everything in the first row is appended
-    document.getElementById("categories").appendChild(firstRow);
-    firstRow.appendChild(firstData);
-    firstRow.appendChild(secondData);
-
-    // this is where everything in the second row is appended
-    document.getElementById("categories").appendChild(secondRow);
-    secondRow.appendChild(secondFirstData);
-    secondRow.appendChild(secondSecondData);
-    secondFirstData.appendChild(inputScores);
-    secondSecondData.appendChild(inputWeight);
-
-    // this is where everything is styled
-    document.getElementById("categories").style.border = "2px solid black";
-    firstData.innerHTML = categoryTitle + " Scores";
-    secondData.innerText = "Percentage";
-    firstData.style.border = "1px dashed black";
-    secondData.style.border = "1px dashed black";
-
-    // this is how the category title input is reset.
-    document.getElementById("categoryTitle").value = "";
 }
 
 /* convertArrayStringToNumber(string) → takes a string of comma separated values (from page) and returns it as an array of
 numbers.  Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each
 item in the array into a number like: array[i] = parseInt(array[i]) */
-function convertArrayStringToNumber(){
-
+function convertArrayStringToNumber(string){
+    var array = [];
+    var buildString = "";
+    for (var i = 0; i < string.length; i++){
+        if(string.substring(i, i+1) != ","){
+            buildString += string.slice(i);
+        } else {
+            array.push(buildString);
+            buildString = "";
+        }
+    }
+    for (var j = 0; j < array.length; j++){
+        parseInt(array[j]);
+    }
 }
 
 // averageArray() → takes an array of numbers and returns the average of those numbers
@@ -74,5 +87,3 @@ the math to determine what the user needs on the final. */
 function calculateGradeNeeded(){
 
 }
-
-// This function makes sure that addRun
