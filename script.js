14563bf7dffff3e02d8eaccf8c0030e56ac08d5c
“sjr-bglobal user.email sophier-b@sbcglobal.netgit config --global user.name “sjr-b <sophier-b@sbcglobal.netgit config --global user.name sjr-b>
2018-10-30 20:57:45 -0700

    figure out how to put the string bits into the array, and then convert them
    you're doing great, it's ok

--- script.js
+++ script.js
@@ -1,45 +1,79 @@
 // NO GLOBAL VARIABLES. EVER. DON'T EVEN THINK ABOUT IT.
 
-// This is the onload function, and happens when the page is loaded.
+// This is the automatic function, and happens when the page is loaded.
 function onLoad(){
     addRow();
 }
 
 // addRow() Adds a new category/weight row (up to 6)
 function addRow(){
-    var row = document.createElement("tr");
-    var firstData = document.createElement("td");
-    var emptyBetween = document.createElement("td");
-    var secondData = document.createElement("td");
-    var categoryTitle = document.getElementsByTagName("input").value;
-    document.getElementById("categories").appendChild(row);
-    row.appendChild(firstData);
-    row.appendChild(emptyBetween);
-    row.appendChild(secondData);
-    firstData.innerHTML = categoryTitle;
-    firstData.innerText = "test";
-    secondData.innerText = "Percentage";
-    firstData.style.backgroundColor = "red";
-    firstData.style.length = "25px";
-    secondData.style.backgroundColor = "red";
+    if (document.getElementById("categories").rows.length < 12){
+        // these are the variables
+        var titleRow = document.createElement("tr");
 
-    var buttonPlace = document.getElementById("firstBreak");
-    var button = document.createElement("button");
-    var buttonMessage = document.createTextNode("Add A Category");
-    document.insertBefore(buttonPlace, button);
-    button.innerHTML = buttonMessage;
+        var firstTitle = document.createElement("td");
+        firstTitle.setAttribute("class", "title");
+        var secondTitle = document.createElement("td");
+        secondTitle.setAttribute("class", "title");
+        if (document.getElementById("categoryTitle").value == ""){
+            var categoryTitle = "Homework";
+        } else {
+            var categoryTitle = document.getElementById("categoryTitle").value;
+        }
 
-    var textInput = document.createElement("input");
-    textInput.setAttribute("type", "text");
-    textInput.setAttribute("value", "");
-    button.insertBefore(button, textInput);
+        var inputRow = document.createElement("tr");
+
+        var firstInputContainer = document.createElement("td");
+        firstInputContainer.setAttribute("class", "inputCont");
+        var secondInputContainer = document.createElement("td");
+        secondInputContainer.setAttribute("class", "inputCont");
+
+        var scoreInput = document.createElement("input");
+        scoreInput.setAttribute("id", categoryTitle + " score");
+        scoreInput.setAttribute("class", "input");
+        var weightInput = document.createElement("input");
+        weightInput.setAttribute("id", categoryTitle + " weight");
+        weightInput.setAttribute("class", "input");
+
+        // this is where everything in the first row is appended
+        titleRow.appendChild(firstTitle);
+        titleRow.appendChild(secondTitle);
+        document.getElementById("categories").appendChild(titleRow);
+
+        // this is where everything in the second row is appended
+        firstInputContainer.appendChild(scoreInput);
+        secondInputContainer.appendChild(weightInput);
+        inputRow.appendChild(firstInputContainer);
+        inputRow.appendChild(secondInputContainer);
+        document.getElementById("categories").appendChild(inputRow);
+
+        // this is where everything is styled.
+        firstTitle.innerHTML = categoryTitle + " Scores";
+        secondTitle.innerText = "Percentage Weight";
+
+        // this is how the category title input is reset.
+        document.getElementById("categoryTitle").value = "";
+    }
 }
 
 /* convertArrayStringToNumber(string) → takes a string of comma separated values (from page) and returns it as an array of
 numbers.  Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each
 item in the array into a number like: array[i] = parseInt(array[i]) */
-function convertArrayStringToNumber(){
-
+function convertArrayStringToNumber(string){
+    var testString = "";
+    var array = [];
+    for(var i = 0; i < string.length; i++){
+        if(string.substring(i, i+1) == ","){
+            for(var j = i; j >= 0; j--){
+                if (string.substring(j, j+1) == "," || j == 0){
+                    var number = string.slice(j, i+1);
+                    testString += number;
+                    array.push(number);
+                }
+            }
+        }
+    }
+    document.getElementById("testResult").innerHTML = testString;
 }
 
 // averageArray() → takes an array of numbers and returns the average of those numbers
